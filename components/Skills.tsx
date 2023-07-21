77beaa956a12b4263cae0715118821a11715fd0e
Adam Fenjiro <afenjiro@mtu.edu>
2023-07-21 13:50:17 -0400

    fix img bug

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -35,7 +35,7 @@ function Skills() {
             <div className="skillsCardContainer">
 
                 <div className="skillCard">
-                    <img className="skillIcon" src="/img/html-icon.png" />
+                    <img className="skillIcon" src="https://adamfenji.github.io/portfolio/img/html-icon.png" />
                     <p>HTML</p>
                 </div>
 
