52d66e83be7646f4d43f2504cdb2bb5eb394dafb
Adam Fenjiro <afenjiro@mtu.edu>
2023-11-20 19:41:55 -0500

    fixed deployment img issue

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -6,7 +6,7 @@ function Connect() {
     return (
         <div id="connectSection" className="connectContainer">
             <div className="insideContainer">
-                <img className="contactImage" src="https://github.com/adamfenji/portfolio/blob/main/img/contact-img.jpeg" />
+                <img className="contactImage" src="https://github.com/adamfenji/portfolio/blob/main/img/contact-img.jpeg?raw=true" />
                 <div className="connectDescription">
                     <h3>Let's connect!</h3>
                     <p>I am currently located in Houghton, MI. However, I am willing to relocate or work remotely!</p>
